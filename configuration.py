3afe8dcedcf3d39eeaf0805e5f298c44ba86ab2a
PriestBaast <arapi.alena@yandex.com>
2023-07-20 19:03:23 +0400

    First

---
+++ configuration.py
@@ -0,0 +1,7 @@
+URL_SERVICE = "https://69137110-d31a-4ecb-ba33-70774cff316e.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
